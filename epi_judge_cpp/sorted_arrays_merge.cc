cccb463d8bb3f9cf2c676ab4605209bf612522d8
Jim Gartland <jim@danube.io>
2021-06-27 17:41:03 +0100

    10.01

--- epi_judge_cpp/sorted_arrays_merge.cc
+++ epi_judge_cpp/sorted_arrays_merge.cc
@@ -2,9 +2,38 @@
 
 #include "test_framework/generic_test.h"
 using std::vector;
+
+struct IteratorCurrentAndEnd
+{
+  bool operator>(const IteratorCurrentAndEnd& that) const 
+  {
+    return *current > *that.current;
+  }
+  vector<int>::const_iterator current;
+  vector<int>::const_iterator end;
+};
+
 vector<int> MergeSortedArrays(const vector<vector<int>>& sorted_arrays) {
-  // TODO - you fill in here.
-  return {};
+  std::priority_queue<IteratorCurrentAndEnd, std::vector<IteratorCurrentAndEnd>, std::greater<>> min_heap;
+
+  // Intialise min-heap with current and end iterators for each array.
+  for (const auto & sorted_array : sorted_arrays)
+    if (!sorted_array.empty())
+      min_heap.emplace(IteratorCurrentAndEnd{sorted_array.cbegin(), sorted_array.cend()});
+
+  std::vector<int> results;
+  while(!min_heap.empty())
+  {
+    auto smallest_array = min_heap.top();
+    min_heap.pop();
+    if (smallest_array.current != smallest_array.end)
+    {
+      results.emplace_back(*smallest_array.current);
+      min_heap.emplace(IteratorCurrentAndEnd{++smallest_array.current, smallest_array.end});
+    }
+  }
+
+  return results;
 }
 
 int main(int argc, char* argv[]) {
