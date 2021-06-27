6f07e59b1326cd7f443d64e8e1c442507b0b17d0
Jim Gartland <jim@danube.io>
2021-06-27 17:41:43 +0100

    .gitignore Visual Studio Code files

--- epi_judge_cpp/sorted_arrays_merge.cc
+++ epi_judge_cpp/sorted_arrays_merge.cc
@@ -5,7 +5,7 @@ using std::vector;
 
 struct IteratorCurrentAndEnd
 {
-  bool operator>(const IteratorCurrentAndEnd& that) const 
+  bool operator>(const IteratorCurrentAndEnd &that) const
   {
     return *current > *that.current;
   }
@@ -13,16 +13,17 @@ struct IteratorCurrentAndEnd
   vector<int>::const_iterator end;
 };
 
-vector<int> MergeSortedArrays(const vector<vector<int>>& sorted_arrays) {
+vector<int> MergeSortedArrays(const vector<vector<int>> &sorted_arrays)
+{
   std::priority_queue<IteratorCurrentAndEnd, std::vector<IteratorCurrentAndEnd>, std::greater<>> min_heap;
 
   // Intialise min-heap with current and end iterators for each array.
-  for (const auto & sorted_array : sorted_arrays)
+  for (const auto &sorted_array : sorted_arrays)
     if (!sorted_array.empty())
       min_heap.emplace(IteratorCurrentAndEnd{sorted_array.cbegin(), sorted_array.cend()});
 
   std::vector<int> results;
-  while(!min_heap.empty())
+  while (!min_heap.empty())
   {
     auto smallest_array = min_heap.top();
     min_heap.pop();
@@ -36,7 +37,8 @@ vector<int> MergeSortedArrays(const vector<vector<int>>& sorted_arrays) {
   return results;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"sorted_arrays"};
   return GenericTestMain(args, "sorted_arrays_merge.cc",
