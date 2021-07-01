d40708398cab59aa022d82fc9b295096cd4e20cd
Jim Gartland <jim@danube.io>
2021-07-01 13:09:06 +0100

    10.03 Sort an almost-sorted array

--- epi_judge_cpp/sort_almost_sorted_array.cc
+++ epi_judge_cpp/sort_almost_sorted_array.cc
@@ -4,16 +4,44 @@
 using std::vector;
 vector<int> SortApproximatelySortedData(
     vector<int>::const_iterator sequence_begin,
-    const vector<int>::const_iterator& sequence_end, int k) {
-  // TODO - you fill in here.
-  return {};
+    const vector<int>::const_iterator &sequence_end, int k)
+{
+
+  std::vector<int> results;
+  std::priority_queue<int, std::vector<int>, std::greater<>> min_heap;
+
+  vector<int>::const_iterator it = sequence_begin;
+  for (int i = 0; i < k; ++i)
+  {
+    min_heap.emplace(*it);
+    ++it;
+  }
+
+  while (it != sequence_end)
+  {
+    min_heap.emplace(*it);
+    results.emplace_back(min_heap.top());
+    min_heap.pop();
+    ++it;
+  }
+
+  // Sequence is exhausted, extract remaining elements
+  while (!min_heap.empty())
+  {
+    results.emplace_back(min_heap.top());
+    min_heap.pop();
+  }
+
+  return results;
 }
-vector<int> SortApproximatelySortedDataWrapper(const vector<int>& sequence,
-                                               int k) {
+vector<int> SortApproximatelySortedDataWrapper(const vector<int> &sequence,
+                                               int k)
+{
   return SortApproximatelySortedData(cbegin(sequence), cend(sequence), k);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"sequence", "k"};
   return GenericTestMain(
