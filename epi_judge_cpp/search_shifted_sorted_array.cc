e6e4904fc49a816fd37669e7def9cff983551837
Jim Gartland <jim@danube.io>
2021-07-02 11:19:59 +0100

    11.03 Search a cyclically sorted array

--- epi_judge_cpp/search_shifted_sorted_array.cc
+++ epi_judge_cpp/search_shifted_sorted_array.cc
@@ -3,12 +3,26 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-int SearchSmallest(const vector<int>& A) {
-  // TODO - you fill in here.
-  return 0;
+int SearchSmallest(const vector<int> &A)
+{
+  int left = 0, right = A.size() - 1;
+
+  while (left < right)
+  {
+    int mid = left + (0.5 * (right - left));
+
+    if (A[mid] > A[right])
+      left = mid + 1;
+    else if (A[mid] < A[right])
+      right = mid;
+  }
+
+  // ends when left == right
+  return left;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(args, "search_shifted_sorted_array.cc",
