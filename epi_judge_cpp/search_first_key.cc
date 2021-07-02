d984c7025f0de8ec5eed990dc0ab33000cb3c911
Jim Gartland <jim@danube.io>
2021-07-02 11:07:18 +0100

    11.01 Search a sorted array for first occurrence of k

--- epi_judge_cpp/search_first_key.cc
+++ epi_judge_cpp/search_first_key.cc
@@ -3,12 +3,28 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-int SearchFirstOfK(const vector<int>& A, int k) {
-  // TODO - you fill in here.
-  return 0;
+int SearchFirstOfK(const vector<int> &A, int k)
+{
+  int left = 0, right = A.size() - 1, result = -1;
+
+  while (left <= right)
+  {
+    int mid = left + (0.5 * (right - left));
+    if (A[mid] > k)
+      right = mid - 1;
+    else if (A[mid] == k)
+    {
+      result = mid;
+      right = mid - 1;
+    }
+    else // A[mid] < k
+      left = mid + 1;
+  }
+  return result;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A", "k"};
   return GenericTestMain(args, "search_first_key.cc", "search_first_key.tsv",
