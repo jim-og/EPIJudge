55b36f51739fc34c336445ead495f5e1c96c50cf
Jim Gartland <jim@danube.io>
2021-07-02 12:39:04 +0100

    11.04 Compute the integer square root

--- epi_judge_cpp/int_square_root.cc
+++ epi_judge_cpp/int_square_root.cc
@@ -1,11 +1,24 @@
 #include "test_framework/generic_test.h"
 
-int SquareRoot(int k) {
-  // TODO - you fill in here.
-  return 0;
+int SquareRoot(int k)
+{
+  int left = 0, right = k;
+  while (left <= right)
+  {
+    // double doesn't work
+    long mid = left + (0.5 * (right - left));
+    long square = mid * mid;
+
+    if (square <= k)
+      left = mid + 1;
+    else
+      right = mid - 1;
+  }
+  return left - 1;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"k"};
   return GenericTestMain(args, "int_square_root.cc", "int_square_root.tsv",
