fa2ae256353f9179a083b656513a33770a2d3770
Jim Gartland <jim@danube.io>
2021-07-10 17:42:00 +0100

    12.05 Find the nearest repeated entries in an array

--- epi_judge_cpp/nearest_repeated_entries.cc
+++ epi_judge_cpp/nearest_repeated_entries.cc
@@ -1,16 +1,33 @@
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 #include "test_framework/generic_test.h"
 using std::string;
 using std::vector;
 
-int FindNearestRepetition(const vector<string>& paragraph) {
-  // TODO - you fill in here.
-  return 0;
+int FindNearestRepetition(const vector<string> &paragraph)
+{
+  std::unordered_map<std::string, int> word_latest_index;
+  int nearest_repeated_distance = std::numeric_limits<double>::max();
+  bool repetition_found = false;
+
+  for (int i = 0; i < paragraph.size(); ++i)
+  {
+    auto it = word_latest_index.find(paragraph[i]);
+    if (it != word_latest_index.end())
+    {
+      nearest_repeated_distance = std::min(nearest_repeated_distance, i - it->second);
+      repetition_found = true;
+    }
+    word_latest_index[paragraph[i]] = i;
+  }
+
+  return repetition_found ? nearest_repeated_distance : -1;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"paragraph"};
   return GenericTestMain(args, "nearest_repeated_entries.cc",
